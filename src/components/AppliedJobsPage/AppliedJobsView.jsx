504aebb8d9d07daa6cf5e73ea6186cb64cda0b21
navneet7716 <navneetsingh969@gmail.com>
2021-12-31 16:51:02 +0530

    added some changes

--- src/components/AppliedJobsPage/AppliedJobsView.jsx
+++ src/components/AppliedJobsPage/AppliedJobsView.jsx
@@ -1,9 +1,38 @@
+import axios from 'axios';
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../redux/UserContext/UserSlice';
 
 function AppliedJobsView() {
+
+    const user = useSelector(selectUser)
+
+    const [appliedJobs, setAppliedJobs] = React.useState([])
+    
+    React.useEffect(()=> {
+        
+        let fetching = true;
+
+        axios.get(`http://localhost:8080/api/applicant/getapplicantbyuserid/${user.id}`, {
+            headers :{
+                Authorization: `Bearer ${
+                    JSON.parse(localStorage.getItem("token")).token
+                  }`
+            }
+        }).then(data => { 
+            setAppliedJobs([...appliedJobs, ...data.data.ListData])
+            console.log(data)
+        
+        })
+        
+
+    }, [])
+
     return (
         <div>
-            View Applied Jobs
+            {
+                appliedJobs && appliedJobs.map(el => <div>{el.id}</div>)
+            }
         </div>
     )
 }
